342d9674289390a49050335aeabb646b0d433ed7
Matthew <matthew.bowman@tutanota.com>
2026-02-16 19:14:18 +0000

    Removed debug

--- budget/lib/Enum/AccountType.php
+++ budget/lib/Enum/AccountType.php
@@ -85,8 +85,6 @@ enum AccountType: string {
      * Check if a string is a valid account type.
      */
     public static function isValid(string $value): bool {
-        var_dump(in_array($value, self::values(), true));
-        die('cobain');
         return in_array($value, self::values(), true);
     }
 }
