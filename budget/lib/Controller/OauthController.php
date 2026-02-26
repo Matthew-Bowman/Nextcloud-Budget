590a60808bbbea94a0dfce74cdebe29fd1cf819b
Matthew <matthew.bowman@tutanota.com>
2026-02-26 20:37:40 +0000

    Beginning of Monzo OAuth impl.

--- budget/lib/Controller/OauthController.php
+++ budget/lib/Controller/OauthController.php
@@ -9,12 +9,19 @@ use OCP\AppFramework\Controller;
 use OCP\AppFramework\Http\TemplateResponse;
 use OCP\IRequest;
 use OCP\Util;
+use OCP\AppFramework\Http\DataResponse;
+use OCP\AppFramework\Http;
+use OCP\Http\Client\IClientService;
 
 class OauthController extends Controller
 {
-    public function __construct(IRequest $request)
+
+    protected $clientService;
+
+    public function __construct(IRequest $request, IClientService $clientService)
     {
         parent::__construct(Application::APP_ID, $request);
+        $this->clientService = $clientService;
     }
 
     /**
@@ -22,8 +29,64 @@ class OauthController extends Controller
      * @NoAdminRequired
      * @NoCSRFRequired
      */
-    public function monzoCallback(): TemplateResponse
+    public function monzoCallback(): DataResponse
     {
-        die('Test');
+        // Var Declarations
+        $code = $this->request->getParam('code');
+        $state = $this->request->getParam('state');
+        $missing = [];
+
+        // Validation Checking
+        if (!isset($code) || $code === null)
+            $missing[] = 'code';
+
+        if (!isset($state) || $state === null)
+            $missing[] = 'status';
+
+        if (count($missing) > 0)
+            return new DataResponse(['message' => "Missing required parameter(s): " . join(', ', $missing)], Http::STATUS_BAD_REQUEST);
+
+
+        // Core Logic
+        $client = $this->clientService->newClient();
+
+        try {
+
+
+            $response = $client->post('https://api.monzo.com/oauth2/token', [
+                "form_params" => [
+                    'grant_type' => 'authorization_code',
+                    'client_id' => 'oauth2client_0000B3UGkFD6uBYvAofHKE',
+                    'client_secret' => 'mnzconf.vYxZQfPP1gPwBIQ+x9Gd1uSU2orZWE/ztv8foC6O/4NW/bnSO9722NgTDRakius9TUtdCW9Ym3q+ryXKEdVryQ==',
+                    'redirect_uri' => 'https://nextcloud.home.lab/apps/budget/oauth/monzo/callback',
+                    'code' => $code,
+                ]
+            ]);
+
+            /**
+             * Now I need to:
+             * ==============
+             * Store the tokens & expiry
+             * Create centralised function for grabbing access token but checking if its expired before doing so and if it it:
+             *  Use refresh token
+             * 
+             * Grab all accounts from the /accounts endpoint
+             *  Use the Monzo provided account ids in the database
+             *  Check if account with that monzo account id exists first though
+             *      Don't store it if so
+             *  Else store them
+             *  Loop through the account IDs and grab all their balances then store it in the db
+             *  Maybe make this into a function because the cron will do the same thing?
+             *  */
+
+            return new DataResponse([
+                'status' => $response->getStatusCode(),
+                'body' => $response->getBody(),
+            ]);
+        } catch (\Throwable $e) {
+            return new DataResponse([
+                'error' => $e->getMessage(),
+            ], 500);
+        }
     }
 }
